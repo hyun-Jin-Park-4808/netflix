e7cf25f41f4111d95db9efb48783a98241e8919b
박현진 <hyunjin@petch.com>
2024-10-21 15:21:11 +0900

    Feat: provider & service 구현

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,63 +1,32 @@
 import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
-import { time } from 'console';
-import { title } from 'process';
-import { isReadable } from 'stream';
 
-interface Movie {
-  id: number;
-  title: string;
-}
+
 
 @Controller('movie')
 export class AppController {
-  private movies: Movie[] = [
-    {
-      id: 1, 
-      title: '해리포터'
-    },
-    {
-      id: 2, 
-      title: '반지의 제왕'
-    }
-  ];
 
-  private idCounter = 3;
-
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+  // 사용하고 싶은 의존성을 정의만 해주면 nestJS에서 알아서 의존성 주입을 해준다. 
 
   @Get()
   getMovies(
     @Query('title') title?: string,
   ) {
-    if(!title) {
-      return this.movies;
-    }
-    return this.movies.filter(m => m.title.startsWith(title));
+    // title 쿼리의 타입이 string 타입인지 검증
+    return this.appService.getManyMovies(title);
   }
 
   @Get(':id')
   getMovie(@Param('id') id: string) {
-    const movie = this.movies.find((m) => m.id === +id);
-    if(!movie) {
-      throw new NotFoundException('존재하지 않는 영화입니다.');
-    }
-    return movie;
+    return this.appService.getMovieById(+id); // +id: string을 number로 바꿔서 넣어준다. 
   }
 
   @Post()
   postMovie(
     @Body('title') title: string,
   ) {
-    const movie: Movie = {
-      id: this.idCounter++,
-      title: title,
-    };
-    this.movies.push(
-      movie,
-    );
-
-    return movie;
+    return this.appService.createMovie(title);
   }
 
   @Patch(':id')
@@ -65,27 +34,13 @@ export class AppController {
     @Param('id') id: string,
     @Body('title') title: string,
   ) {
-    const movie = this.movies.find(m => m.id === +id);
-
-    if(!movie) {
-      throw new NotFoundException('존재하지 않는 ID의 영화입니다');
-    }
-    Object.assign(movie, {title});
-
-    return movie;
+    return this.appService.updateMovie(+id, title);
   }
 
   @Delete(':id')
   deleteMovie(
     @Param('id') id: string,
   ) {
-    const movieIndex = this.movies.findIndex(m => m.id === +id);
-
-    if(movieIndex === -1) {
-      throw new NotFoundException('존재하지 않는 ID의 영화입니다');
-    }
-    this.movies.splice(movieIndex, 1);
-
-    return id;
+    return this.appService.deleteMovie(+id);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService], 
+  // 1. AppService에 @Injectable() 데코레이터를 달아, 해당 클래스를 IoC에서 관리하도록 명시해준다.
+  // 2. providers 배열을 통해 IoC가 AppService 인스턴스가 필요함을 인지하고 인스턴스화하고 추출해서 위 컴트롤러에 넣어준다. 
 })
 export class AppModule {}
