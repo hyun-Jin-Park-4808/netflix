d4219a1f43a0bc2308ed286d5f1b3b068cc94942
박현진 <hyunjin@petch.com>
2024-10-22 13:55:06 +0900

    refactor

--- src/movie/entity/movie.entity.ts
+++ src/movie/entity/movie.entity.ts
@@ -1,12 +1,6 @@
-import { Exclude, Expose, Transform } from "class-transformer";
 
 export class Movie {
     id: number;
-    title: string; 
-    @Transform(
-        // ({value}) => 'code factory' genre 값 전부 code facotory로 변환해준다.
-        ({value}) => value.toString().toUpperCase(), // 대문자로 변경해준다. 
-    )
+    title: string;
     genre: string;
-
 }
