b361ea488b4614a7873db0069e4a43d5c4860cc5
박현진 <hyunjin@petch.com>
2025-07-09 22:53:20 +0900

    feat: database 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,38 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 import { MovieModule } from './movie/movie.module';
 
 @Module({
-  imports: [MovieModule], // 다른 모듈을 이 모듈로 불러들일 때 사용, nest g로 생성한 모듈은 자동 주입 된다. 
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // 환경변수 검증
+      validationSchema: Joi.object({
+        ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_TYPE: Joi.string().valid('postgres').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DB_TYPE') as 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [],
+        synchronize: true, // 실서버에서는 false로 해야 한다.
+      }),
+      inject: [ConfigService], // IoC 컨테이너에서 ConfigService를 DI 해줘야한다고 알려주기 위함
+    }),
+    MovieModule,
+  ], // 다른 모듈을 이 모듈로 불러들일 때 사용, nest g로 생성한 모듈은 자동 주입 된다.
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // dto에서 정의하지 않는 속성은 들어갈 수 없게 해준다. 에러는 발생 x, 기본은 false
-    forbidNonWhitelisted: true, // whitelist 걸렸을 때 에러 발생시킨다. 
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // dto에서 정의하지 않는 속성은 들어갈 수 없게 해준다. 에러는 발생 x, 기본은 false
+      forbidNonWhitelisted: true, // whitelist 걸렸을 때 에러 발생시킨다.
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
