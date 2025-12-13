ea9af01c5f6eb759383aff21514c977938a9ffd9
박현진 <hyunjin@petch.com>
2025-12-13 17:16:15 +0900

    feat: Mapped Types 적용

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,3 +1,4 @@
+import { PartialType } from '@nestjs/mapped-types';
 import {
   ArrayNotEmpty,
   IsArray,
@@ -6,24 +7,28 @@ import {
   IsOptional,
   IsString,
 } from 'class-validator';
+import { createMovieDto } from './create-movie.dto';
 
-export class UpdateMovieDto {
-  @IsNotEmpty() // title key 값을 입력하면 value 값이 비면 안 된다.
-  @IsOptional() // title을 Key 값으로 안 넣는 건 허용된다.
-  @IsString()
-  title?: string;
+export class UpdateMovieDto extends PartialType(createMovieDto) {}
 
-  @IsArray()
-  @ArrayNotEmpty()
-  @IsNumber({}, { each: true }) // each: true :  validation 옵션, 배열 모든 요소가 number 타입이어야 한다.
-  genreIds?: number[];
+// export class UpdateMovieDto {
+//   @IsNotEmpty() // title key 값을 입력하면 value 값이 비면 안 된다.
+//   @IsOptional() // title을 Key 값으로 안 넣는 건 허용된다.
+//   @IsString()
+//   title?: string;
 
-  @IsNotEmpty()
-  @IsOptional()
-  detail?: string;
+//   @IsArray()
+//   @ArrayNotEmpty()
+//   @IsNumber({}, { each: true }) // each: true :  validation 옵션, 배열 모든 요소가 number 타입이어야 한다.
+//   genreIds?: number[];
 
-  @IsNotEmpty()
-  @IsOptional()
-  @IsNumber()
-  directorId?: number;
-}
+//   @IsNotEmpty()
+//   @IsString()
+//   @IsOptional()
+//   detail?: string;
+
+//   @IsNotEmpty()
+//   @IsOptional()
+//   @IsNumber()
+//   directorId?: number;
+// }
