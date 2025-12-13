a7ce0b09b15abe18bb01ab98c8fe7ac71a08bde0
박현진 <hyunjin@petch.com>
2025-12-13 17:20:41 +0900

    feat: serialization 적용

--- src/common/entity/base-table.entity.ts
+++ src/common/entity/base-table.entity.ts
@@ -1,12 +1,16 @@
+import { Exclude } from 'class-transformer';
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 
 export class BaseTable {
   @CreateDateColumn()
+  @Exclude() // 해당 컬럼 조회할 때 제외
   createdAt: Date;
 
   @UpdateDateColumn()
+  @Exclude()
   updatedAt: Date;
 
   @VersionColumn()
+  @Exclude()
   version: number;
 }
