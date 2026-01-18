c8d0214be5b6f3ac833b6ebb170dc0b28edff89c
박현진 <hyunjin@petch.com>
2026-01-18 19:22:13 +0900

    test: findUser 테스트하기

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,7 +1,8 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from './user.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
+import { UserService } from './user.service';
 
 const mockUserRepository = {
   findOne: jest.fn(),
@@ -12,7 +13,7 @@ const mockUserRepository = {
 };
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -25,10 +26,58 @@ describe('UserService', () => {
       ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  describe("findAll", () => {
+    it('should return all users', async () => {
+      const users = [
+        {
+          id: 1,
+          email: 'user1@example.com',
+        },
+        {
+          id: 2,
+          email: 'user2@example.com',
+        },
+      ];
+      mockUserRepository.find.mockResolvedValue(users); // Promise로 반환
+
+      const result = await userService.findAll();
+      expect(result).toEqual(users);
+      expect(mockUserRepository.find).toHaveBeenCalled();
+    });
+  });
+
+  describe("findOne", () => {
+    it('should return one user by id', async () => {
+      const user = {
+        id: 1,
+        email: 'user1@example.com',
+      };
+      mockUserRepository.findOne.mockResolvedValue(user);
+
+      jest.spyOn(mockUserRepository, 'findOne').mockResolvedValue(user);
+
+      const result = await userService.findOne(1);
+
+      expect(result).toEqual(user);
+      expect(mockUserRepository.findOne).toHaveBeenCalledWith({
+        where: { id: 1 },
+      });
+    });
+
+    it('should throw a NotFoundException if user not found', async () => {
+      jest.spyOn(mockUserRepository, 'findOne').mockResolvedValue(null);
+
+      await expect(userService.findOne(999)).rejects.toThrow(NotFoundException);
+      expect(mockUserRepository.findOne).toHaveBeenCalledWith({
+        where: { id: 999 },
+      });
+    });
   });
 });
